c1121f88393b13c85174f5954d6f169548ad8ee5
Collins-Ibe <ccollinsibe@hotmail.com>
2022-12-24 05:11:33 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+            I thank God it works like a charm, even if not I still thank Him!!!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React,
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
